280e1b001ceff24c8538f960850db4a6b45d4d89
itko <ittokor@gmail.com>
2017-03-26 15:46:43 -0400

    Added insert_link function (still not working). Modified the get_text function

--- get_text.js
+++ get_text.js
@@ -1,15 +1,30 @@
-function get_text(jquery_object) {
+// Takes a jquery object and returns all text inside
+// (excluding scripts and styles text)
+function get_text(object) {
+	// Clone
+	objectClone = object.clone();
 	// Remove scripts
-	var noScripts = jquery_object.first().find('script').remove().end()
-	var noStyles = noScripts.find('style').remove().end()
-	var text = noStyles.text()
+	var noScripts = objectClone.first().find('script').remove().end();
+	var noStyles = noScripts.first().find('style').remove().end();
+	var text = noStyles.first().text();
 	return text
 }
 
+// Replaces a given string in an object with a url
+// (with the string as the link text)
+function insert_link(object, string, url) {
+	searchString = '/'+string+'/ig'
+	alert(searchString)
+	alert(url)
+	var html = object.html().replace(searchString,url);
+	console.log(html);
+	// object.html(html);
 
+}
 
 jQuery(document).ready(function() {
-	allElements = jQuery('*').clone();
-	text = get_text(allElements)
-	console.log(text)
+	allElements = jQuery('*');
+	text = get_text(allElements);
+	console.log(text);
+	insert_link(allElements.find('body'), 'February', 'Google')
 });
