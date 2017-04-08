980d658eb7192afd8bb1394a2c16cb471b3406ad
Anatoly <anatolymakarov@gmail.com>
2017-04-08 13:24:52 -0400

    added getStringsToLinks and keywordsToDate

--- text_to_date.js
+++ text_to_date.js
@@ -11,18 +11,27 @@ $(document).ready(function(){
         var x = $("#inTXT").val();
 		//alert(x);
 		
-		var match1 = output(x);
+		var match1 = main(x);
 	
 		var outString = "";
 		for (var i = 0; i <= match1.index.length-1; i++){
-			outString = outString + "Index: " + match1.index[i] + " Match: " + match1.value[i] + " RegEx: " + match1.regExType[i] + "\n";
+			//outString = outString + "Index: " + match1.index[i] + " Match: " + match1.value[i] + " RegEx: " + match1.regExType[i] + "\n";
+			outString = outString + "Index: " + match1.index[i] + " Match: " + match1.string[i] + " All match: " + match1.allStrings[i] + "\n";
 		}
         $("#outTXT").val(outString);
     });
 });
+function main(x){
+	var match = parseDatesFromString(x)
+	var strToLinks = getStringsToLinks(match)
+	
+	return strToLinks
+}
 
 
-function output(x){
+//Input: (String) Text which contains dates
+//Output: (Array) Date keywords and their index within the string
+function parseDatesFromString(x){
 	var regExList = new Array()
 	regExList[0] = /[0-9][0-9][/-][0-9][0-9][/-][0-9][0-9]/g
 	regExList[1] = /(monday|tuesday|wednesday|thursday|friday|saturday|sunday)/gi
@@ -47,8 +56,47 @@ function output(x){
 			k++
 		}
 		
-		
 	}
 	return match
 	//alert("S")
 }
+
+function getStringsToLinks(match){
+	
+	var strToLinks = {string: new Array(), index: new Array(), allStrings: new Array()}
+	
+	for (var i = 0; i <= match.index.length-1; i++){
+		strToLinks.string[i] = match.value[i]
+		strToLinks.index[i] = match.index[i]
+		strToLinks.allStrings[i] = match.value[i] //need to modify in future. Values separated by "|". ex. "saturday|tomorrow|3pm"
+	}
+	
+	
+	return strToLinks
+	
+}
+
+function keywordsToDate(strToLinks){
+	
+	//look into JodaTime
+	for (var i = 0; i <= strToLinks.index.length-1; i++){
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
